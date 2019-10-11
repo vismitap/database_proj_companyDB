157b02824cae59cb16f6e97c540ad3ad17f4a52d
vismitap <47056243+vismitap@users.noreply.github.com>
2019-10-11 20:23:37 +0530

    actual working using javascript!

---
+++ index.js
@@ -0,0 +1,56 @@
+      
+         document.getElementById("emp").addEventListener("click",function(){
+             document.querySelector('.bg-modal1').style.display='flex';
+         });
+       
+       
+        document.querySelector('.close1').addEventListener('click',function(){
+            document.querySelector('.bg-modal1').style.display='none';
+        });
+        
+        
+              document.getElementById("proj").addEventListener("click",function(){
+             document.querySelector('.bg-modal2').style.display='flex';
+         });
+       
+       
+        document.querySelector('.close2').addEventListener('click',function(){
+            document.querySelector('.bg-modal2').style.display='none';
+        });
+        
+       
+            
+             document.getElementById("dept").addEventListener("click",function(){
+             document.querySelector('.bg-modal3').style.display='flex';
+         });
+       
+       
+        document.querySelector('.close3').addEventListener('click',function(){
+            document.querySelector('.bg-modal3').style.display='none';
+        });
+        
+       
+            
+              document.getElementById("man").addEventListener("click",function(){
+             document.querySelector('.bg-modal4').style.display='flex';
+         });
+       
+       
+        document.querySelector('.close4').addEventListener('click',function(){
+            document.querySelector('.bg-modal4').style.display='none';
+        });
+        
+       
+            
+              document.getElementById("dep").addEventListener("click",function(){
+             document.querySelector('.bg-modal5').style.display='flex';
+         });
+       
+       
+        document.querySelector('.close5').addEventListener('click',function(){
+            document.querySelector('.bg-modal5').style.display='none';
+        });
+        
+       
+        
+      
